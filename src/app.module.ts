29887c2909dbd808176e88bb1415dd671d9198df
Intezar Alam <tanjavoorathefe@gmail.com>
2024-10-21 20:18:00 +0530

    basic and advanced nest js backend framework done with project

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './user/users.module';
 import { TweetModule } from './tweet/tweet.module';
 import { TicketsModule } from './tickets/tickets.module';
 import { AddressModule } from './address/address.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, TweetModule, TicketsModule, AddressModule], //import all created module
+  imports: [UsersModule, TweetModule, TicketsModule, AddressModule, AuthModule], //import all created module
   controllers: [AppController],
   providers: [AppService],
 })
